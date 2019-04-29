73555c90c73026c9f4835fd6fab3fc2a52c4b34d
노경모 <kyeongmo2@gmail.com>
2019-04-30 00:10:45 +0900

    Add get-a-gif-image api that returns one gif image using a given id(parameter)

--- routes.js
+++ routes.js
@@ -11,13 +11,13 @@ router.get('/gif/all', (req, res) => {
     res
       .status(400)
       .send({
-        message: 'Page query parameter should be given.',
+        message: 'page query parameter should be given.',
       })
   } else if (page < 1) {
     res
       .status(400)
       .send({
-        message: 'Page query parameter should be bigger than 0.',
+        message: 'page query parameter should be bigger than 0.',
       })
   }
 
@@ -29,6 +29,35 @@ router.get('/gif/all', (req, res) => {
     .send({
       data: data.slice(startIndex, endIndex),
     })
+})
+
+// GET A GIF IMAGE WITH A GIVEN ID
+router.get('/gif/:id', (req, res) => {
+  const { id } = req.params
+
+  if (!id) {
+    res
+      .status(400)
+      .send({
+        message: 'id parameter should be given.',
+      })
+  }
+
+  const foundGif = data.find((gif) => gif.id === id)
+
+  if (foundGif) {
+    res
+      .status(200)
+      .send({
+        data: foundGif,
+      })
+  } else {
+    res
+      .status(400)
+      .send({
+        message: 'The gif image with a given id could not be found.',
+      })
+  }
 })
 
 module.exports = router
